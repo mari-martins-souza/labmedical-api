b5a4b059dc2afbe7ecf338589e15af28b2f745ff
Mariana Martins <marih_ms@hotmail.com>
2024-07-23 19:53:19 -0300

    first commit

---
+++ src/main/java/tech/lab365/labmedical/LaBmedicalApplication.java
@@ -0,0 +1,13 @@
+package tech.lab365.labmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBmedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/lab365/labmedical/LaBmedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.lab365.labmedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBmedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
